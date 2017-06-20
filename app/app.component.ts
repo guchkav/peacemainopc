ee5f8fe240f01ee0c6e26d4eebd9a92b6d620941
Benkatesh Panyam <whenkatezh@gmail.com>
2017-06-21 00:33:56 +0530

    Included node-modules and typings dependencies

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello World</h1>'
+  template: '<h1>Hello World Its Changed</h1>'
 })
 export class AppComponent { }
