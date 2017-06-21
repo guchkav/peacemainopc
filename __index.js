aaa314ee7b9c1ea71c0f80647f1a31c099917aeb
Venkatesh Panyam <whenkatezh@gmail.com>
2017-06-21 18:40:47 +0530

    Included Mobile Menu

--- __index.js
+++ __index.js
@@ -1,5 +1,9 @@
 function showMenu(){
 	var menuID = document.getElementById('header-menu-id');
-	menuID.style.display = "block";
-	menuID.className +=' header-menu-active';
+	if( menuID.classList.contains('header-menu-active')){	
+		menuID.classList.remove('header-menu-active');			
+		
+	} else{
+		menuID.classList.add('header-menu-active');
+	}
 }
